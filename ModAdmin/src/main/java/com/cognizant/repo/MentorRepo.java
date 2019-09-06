946e2fc8ecb5ecc8b5a0e0aa23bc7d1761bf7929
jontysachdeva <jontysachdeva007@gmail.com>
2019-09-07 01:30:18 +0530

    initial commit

--- ModAdmin/src/main/java/com/cognizant/repo/MentorRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cognizant.entity.MentorEntity;
-
-public interface MentorRepo extends JpaRepository<MentorEntity, Integer> {
-
-}
